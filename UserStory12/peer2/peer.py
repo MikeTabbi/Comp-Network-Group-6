89f8faeec451fb7d1ce18ae784739dfe1e89fa80
Q'Lae <77710901+qlae@users.noreply.github.com>
2025-04-13 19:04:32 +0000

    multi-peer download working

---
+++ UserStory12/peer2/peer.py
@@ -0,0 +1,18 @@
+from flask import Flask, send_file, request
+import os
+
+app = Flask(__name__)
+PEER_PORT = 5002  
+SHARED_DIR = "shared"
+
+@app.route('/get_chunk')
+def get_chunk():
+    chunk_name = request.args.get('name')
+    chunk_path = os.path.join(SHARED_DIR, chunk_name)
+    if os.path.exists(chunk_path):
+        return send_file(chunk_path, as_attachment=True)
+    else:
+        return "Chunk not found", 404
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PEER_PORT)
